aac30bd6a92ac42ad589f1378cf7de57da52a5a0
Morgan Matney <morgan.matney89@gmail.com>
2020-11-25 21:47:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
 
   get '/articles/:id/edit' do 
     id = params[:id]
-    @article = Article.find_by(id: id)
+    @article = Article.find(params[:id])
 
     erb :edit
   end 
